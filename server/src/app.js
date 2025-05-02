53fbae5fc531bd0c5e6e2233d22a4d251e117bf4
prince kumar <princekumarsingh851212@gmail.com>
2025-05-02 18:54:01 +0530

    app.use(static())

--- server/src/app.js
+++ server/src/app.js
@@ -20,7 +20,7 @@ app.use(cors({
 app.use(helmet());
 app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
-app.use(express.static("public"))
+app.use(express.static(path.join(__dirname,"./public")))
 app.use(cookieParser())
 
 if (process.env.NODE_ENV === "development") {
